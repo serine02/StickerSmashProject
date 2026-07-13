efd0975622ec890621309dc5b129916cc4a57c89
serine02 <marizgilhang104@gmail.com>
2026-07-13 14:53:44 +0800

    stickersmash

---
+++ app/(tabs)/about.tsx
@@ -0,0 +1,22 @@
+import { Text, View, StyleSheet } from 'react-native';
+
+export default function AboutScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Debbie Mariz Gilhang</Text>
+      <Text style={styles.text}>BSIT BLOCK 3A</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    color: '#fff',
+  },
+});
