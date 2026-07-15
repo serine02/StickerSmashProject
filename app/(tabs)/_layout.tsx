c70d28abbeb251ebb186f3b284771e500bd0d8bc
serine02 <marizgilhang104@gmail.com>
2026-07-15 14:10:53 +0800

    add profile

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -37,6 +37,19 @@ export default function TabLayout() {
           ),
         }}
       />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={focused ? "person-outline" : "person-outline"}
+              color={color}
+              size={24}
+            />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
